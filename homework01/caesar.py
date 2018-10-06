b93924845e6f0b2e7630ff2c407d0080642387d5
heidamn <damnsholomov@gmail.com>
2018-10-06 12:19:22 +0300

    код более читабельный

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,15 +11,15 @@ def encrypt_caesar(plaintext):
     >>> encrypt_caesar("")
     ''
     """
-    ciphertext=''
-    n=0
-    for i in range (len(plaintext)):
-    	n=ord(plaintext[i])
-    	if 'a'<=plaintext[i]<='z' or 'A'<=plaintext[i]<='Z':
-    		n+=3
-    		if ord('Z')<n<ord("a") or n>ord('z'):
-    			n-=26
-    	ciphertext+=chr(n)
+    ciphertext = ''
+    n = 0
+    for i in range(len(plaintext)):
+        n = ord(plaintext[i])
+        if 'a' <= plaintext[i] <= 'z' or 'A' <= plaintext[i] <= 'Z':
+            n += 3
+            if ord('Z') < n < ord("a") or n > ord('z'):
+                n -= 26
+        ciphertext += chr(n)
     return ciphertext
 
 
@@ -36,13 +36,13 @@ def decrypt_caesar(ciphertext):
     >>> decrypt_caesar("")
     ''
     """
-    plaintext=''
-    n=0
-    for i in range (len(ciphertext)):
-    	n=ord(ciphertext[i])
-    	if 'a'<=ciphertext[i]<='z' or 'A'<=ciphertext[i]<='Z':
-    		n-=3
-    		if ord('Z')<n<ord("a") or n<ord('A'):
-    			n+=26
-    	plaintext+=chr(n)
+    plaintext = ''
+    n = 0
+    for i in range(len(ciphertext)):
+        n = ord(ciphertext[i])
+        if 'a' <= ciphertext[i] <= 'z' or 'A' <= ciphertext[i] <= 'Z':
+            n -= 3
+            if ord('Z') < n < ord("a") or n < ord('A'):
+                n += 26
+        plaintext += chr(n)
     return plaintext
