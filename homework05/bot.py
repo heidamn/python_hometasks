e9d57ca04576b4a4e373b4aeb099e9c97b2e29ce
heidamn <heidamn@gmail.com>
2018-12-07 00:58:30 +0300

    реализован поиск всех занятий в неделю, bugs fixed" z

--- homework05/bot.py
+++ homework05/bot.py
@@ -1,6 +1,7 @@
 import requests
 import config
 import telebot
+from time import sleep
 from bs4 import BeautifulSoup
 
 
@@ -22,11 +23,11 @@ def get_page(group, week=''):
 @bot.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
 def get_schedule(message):
     """ Получить расписание на указанный день """
-    print(message.text)
     try:
         day, group, week = message.text.split()
+        week = int(week) % 2
     except:
-        week = '0'
+        week = 0
         day, group = message.text.split()
     days = {'/monday': '1day', '/tuesday': '2day', '/wednesday': '3day', '/thursday': '4day', '/friday': '5day', '/saturday': '6day', '/sunday': '7day'}
     web_page = get_page(group)
@@ -52,10 +53,10 @@ def get_schedule(message):
         rooms_list = [room.dd.text for room in rooms_list]
         resp = ''
         for time, location, room, lession in zip(times_list, locations_list, rooms_list, lessons_list):
-            if week == '0':
+            if week == 0:
                 resp += '<b>{}</b>, {}, {} {}\n'.format(time, location, room, lession)
-            elif week == '1':
-                if lession.find('нечетная неделя') != -1:
+            elif week == 1:
+                if lession.find('нечетная неделя') != -1 or lession.find('нечетная неделя') == -1:
                     resp += '<b>{}</b>, {},{} {}\n'.format(time, location, room, lession)
             else:
                 if lession.find('нечетная неделя') == -1:
@@ -80,8 +81,49 @@ def get_tommorow(message):
 @bot.message_handler(commands=['all'])
 def get_all_schedule(message):
     """ Получить расписание на всю неделю для указанной группы """
-    
-    pass
+    try:
+        _, group, week = message.text.split()
+        week = int(week) % 2
+    except:
+        _, group = message.text.split()
+        week = 0
+    days = ['1day', '2day', '3day', '4day', '5day', '6day', '7day']
+    days2 = ['Понедельник','Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+    web_page = get_page(group)
+    soup = BeautifulSoup(web_page, "html5lib")
+    for num,day in enumerate(days):
+        # Получаем таблицу с расписанием на день
+        bot.send_message(message.chat.id, days2[num])
+        schedule_table = soup.find("table", attrs={"id": day})
+        if not schedule_table:
+            bot.send_message(message.chat.id, 'День свободен!')
+        else:
+            # Время проведения занятий
+            times_list = schedule_table.find_all("td", attrs={"class": "time"})
+            times_list = [time.span.text for time in times_list]
+            # Место проведения занятий
+            locations_list = schedule_table.find_all("td", attrs={"class": "room"})
+            locations_list = [room.span.text for room in locations_list]
+            # Название дисциплин и имена преподавателей
+            lessons_list = schedule_table.find_all("td", attrs={"class": "lesson"})
+            lessons_list = [lesson.text.split('\n\n') for lesson in lessons_list]
+            lessons_list = [', '.join([info for info in lesson_info if info]) for lesson_info in lessons_list]
+            # Аудитория
+            rooms_list = schedule_table.find_all("td", attrs={"class": "room"})
+            rooms_list = [room.dd.text for room in rooms_list]
+            resp = ''
+            for time, location, room, lession in zip(times_list, locations_list, rooms_list, lessons_list):
+                if week == 0:
+                    resp += '<b>{}</b>, {}, {} {}\n'.format(time, location, room, lession)
+                elif week == 1:
+                    if lession.find('нечетная неделя') != -1 or lession.find('нечетная неделя') == -1:
+                        resp += '<b>{}</b>, {},{} {}\n'.format(time, location, room, lession)
+                else:
+                    if lession.find('нечетная неделя') == -1:
+                        resp += '<b>{}</b>, {},{} {}\n'.format(time, location, room, lession)
+            bot.send_message(message.chat.id, resp, parse_mode='HTML')
+            sleep(0.1)
+
 
 
 if __name__ == '__main__':
