f6f95a8148a181cce3d0053fc32ec6f3ea631ee7
heidamn <heidamn@gmail.com>
2019-02-21 18:13:46 +0300

    вроде работает, но не очень"

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,93 @@
+import math
+import csv
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha):
-        pass
+    def __init__(self, alpha = 0.05):
+        self.alpha = alpha
+        self.table = {}
+        self.labels = []
+        self.labels_part = []
 
     def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
-        pass
+        """ Fit Naive Bayes classifier according to X, y.
+        X: Данные, которые обрабатываем
+        у: Классы, которым принадлежат данные"""
+        for i,msg in enumerate(X):
+            msg.split()
+            self.labels = [i for i in set(y)]
+            self.labels_part = [y.count(label) / len(y) for label in self.labels]
+            for word in msg:
+                if not self.table.get(word):
+                    self.table.update({word: [[0 for _i in self.labels], [0 for _i in self.labels]]})
+                else:
+                    self.table[word][0][self.labels.index(y[i])] += 1
+        d = len(self.table)
+        n_labels = [0 for _i in self.labels]
+        for i in range(len(n_labels)):
+            for word in self.table:
+                n_labels[i] += self.table[word][0][i]
+        print (n_labels, self.labels_part, d, self.labels, self.table['g'], sep="\n")
+        for word in self.table:
+            for i in range(len(self.table[word][1])):
+                self.table[word][1][i] = (self.alpha + self.table[word][0][i]) / (d + n_labels[i])
+        print(self.table['q'])
+
+
 
     def predict(self, X):
-        """ Perform classification on an array of test vectors X. """
-        pass
+        """ Perform classification on an array of test vectors X.
+        Вычисление класса по входящим данным"""
+        y = []
+        for msg in X:
+            predicted_nums = []
+            msg.split()
+            for i in range (len(self.labels)):
+                predicted_num = math.log(self.labels_part[i])
+                for word in msg:
+                    if self.table.get(word):
+                        predicted_num += math.log(self.table[word][1][i])
+                predicted_nums.append(predicted_num)
+            y.append(self.labels[predicted_nums.index(max(predicted_nums))])
+        return y
 
     def score(self, X_test, y_test):
-        """ Returns the mean accuracy on the given test data and labels. """
-        pass
+        """ Returns the mean accuracy on the given test data and labels.
+        Проверка вероятности совпадения
+        X_test: Данные, которые проверяем
+        y_test: Классы, которым должны принадлежать Проверяемые Данные"""
+        part = 0
+        y = self.predict(X_test)
+        for i in range(len(y_test)):
+            if y[i] == y_test[i]:
+                part += 1
+        part /= len(y_test)
+        return part
 
+with open("data/SMSSpamCollection") as f:
+    data = list(csv.reader(f, delimiter="\t"))
+import string
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
+X, y = [], []
+for target, msg in data:
+    X.append(msg)
+    y.append(target)
+X = [clean(x).lower() for x in X]
+X_train, y_train, X_test, y_test = X[:3900], y[:3900], X[3900:], y[3900:]
+model = NaiveBayesClassifier()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+model = Pipeline([
+    ('vectorizer', TfidfVectorizer()),
+    ('classifier', MultinomialNB(alpha=0.05)),
+])
+
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
---
+++ homework06/scrapper.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+def extract_news(parser):
+    """ Extract news from a given web page """
+    news_list = []
+    tbl_list = parser.table.findAll('table')
+    posts = tbl_list[1].findAll("tr", attrs={'class': 'athing'})
+    postsinfo = tbl_list[1].findAll("tr", attrs={'class': ''})
+    for i in range(30):
+        comment = postsinfo[i].findAll('a')[-1].text.split()
+        url = posts[i].findAll('a', attrs={'class': 'storylink'})[0]['href']
+        if url[:4] != "http":
+            url = "https://news.ycombinator.com/" + url
+        if len(comment) == 1:
+            comment = 0
+        else:
+            comment = int(comment[0])
+        post = {
+            'author': postsinfo[i].findAll('a', attrs={'class': 'hnuser'})[0].text,
+            'comments': comment,
+            'points': int(postsinfo[i].findAll('span', attrs={'class': 'score'})[0].text.split(' ')[0]),
+            'title': posts[i].findAll('a')[1].text,
+            'url': url
+        }
+        news_list.append(post)
+    return news_list
+
+
+def extract_next_page(parser):
+    """ Extract next page URL """
+    tbl_list = parser.table.findAll('table')
+    next_page = tbl_list[1].findAll("tr", attrs={'class': ''})[30].a['href']
+    return next_page
+
+
+def get_news(url, n_pages=1):
+    """ Collect news from a given web page """
+    news = []
+    while n_pages:
+        print("Collecting data from page: {}".format(url))
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        news_list = extract_news(soup)
+        next_page = extract_next_page(soup)
+        url = "https://news.ycombinator.com/" + next_page
+        news.extend(news_list)
+        n_pages -= 1
+        if n_pages > 0:
+            time.sleep(30)
+    return news
+
